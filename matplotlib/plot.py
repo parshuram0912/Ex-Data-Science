5bae884e8d52cdc27804fe1e76e14b373ad21534
parshuram <parshuramovi11@gmail.com>
2026-08-03 07:29:15 +0530

    to learn the plot of chart

---
+++ matplotlib/plot.py
@@ -0,0 +1,15 @@
+# import matplotlib.pyplot as plt
+# cars=["Alto","celerio","swift","Astar","brezza","victriou"]
+# sales=[2000,6000,4500,6400,2300,4800]
+# plt.plot(cars,sales)
+# plt.xlabel("cars module")
+# plt.ylabel("unit")
+# plt.title("sales comparision")
+# plt.show()
+import matplotlib.pyplot as plt
+years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010]
+runs =  [500, 700, 1100, 1500, 1800, 1200, 1700, 1300, 900, 1500]
+plt.bar=(years,runs)
+plt.xlabel="Years"
+plt.ylabel="Runs that score" 
+plt.show()
